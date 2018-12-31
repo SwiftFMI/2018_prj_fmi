8b0a8a2ef94eac8e0cbcf8f1b1291a00ac37e518
Yalishanda <yalishanda@yalishandas-mac.local>
2018-12-31 15:59:35 +0200

    Създадена папка с проекта и добавени първите прототипи на View-тата

---
+++ FMI App/FMI App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FMI App
+//
+//  Created by Yalishanda on 31.12.18.
+//  Copyright © 2018 fmi-swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
