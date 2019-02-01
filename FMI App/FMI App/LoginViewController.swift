827d3241d733f4427c7fef23c296ba12a7972799
allexks <ani1802@abv.bg>
2019-02-01 12:42:36 +0200

    валидация при логин, работеща заявка за логин to do: да се запазва информацията за потребителя (интеграция с Core Data) to do: fix segue

--- FMI App/FMI App/LoginViewController.swift
+++ FMI App/FMI App/LoginViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     
     @IBOutlet var roundedViewsCollection: [UIView]!
+    
+    @IBOutlet weak var errorLabelLogin: UILabel!
     
     let viewsCornerRadius : CGFloat = 30
     
@@ -22,9 +25,32 @@ class LoginViewController: UIViewController {
         for view in roundedViewsCollection {
             view.layer.cornerRadius = viewsCornerRadius
         }
+        
+        // TODO: automatically load user data if available; integration with Core Data
     }
     
     @IBAction func didTapToEndInput(_ sender: Any) {
         view.endEditing(true)
+    }
+    @IBAction func onTapLoginWithEmail(_ sender: Any) {
+        guard let email = emailField.text, let password = passwordField.text
+            else {
+            errorLabelLogin.text = "Моля попълнете полетата за влизане чрез имейл!"
+            errorLabelLogin.alpha = 1
+            return
+        }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self]
+            (authDataResult, error) in
+            guard let user = authDataResult else {
+                self?.errorLabelLogin.text = "Невалидни имейл или парола!"
+                self?.errorLabelLogin.alpha = 1
+                return
+            }
+            
+            self?.errorLabelLogin.alpha = 0
+            // TODO: save data
+            // TODO: fix segue
+        }
     }
 }
